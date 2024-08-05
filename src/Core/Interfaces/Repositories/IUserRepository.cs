8659a6ed0f95b1ae68f7256ff8c52eb72691a83c
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-05 08:54:29 -0300

    Update and rename src/Core/Interfaces/Database.cs to src/Core/Interfaces/Repositories/IUserRepository.cs

---
+++ src/Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+namespace Core.Interfaces.Repositories
+{
+  public interface IUserRepository
+  {
+    IEnumerable<UsuarioModel> GetUsers();
+    UsuarioModel GetUserById(Guid userId);
+    void InsertUser(UsuarioModel user);
+    void UpdateUser(UsuarioModel user);
+    void DeleteUserById(Guid idUser);   
+  }
+}

b6146b518b8ff6f378bda5cd814deb8b03a3e2f8
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-05 23:20:20 -0300

    feat: create pedido model

--- src/Core/Model/PedidoModel.cs
+++ src/Core/Model/PedidoModel.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Negocio.Model
 {
-    internal class PedidoModel
+    public class PedidoModel
     {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public ICollection<ItemPedidoModel> itemPedidos { get; set; }
+
     }
 }

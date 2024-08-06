378bd06089600bb5b2a21a222550a57b2e9be931
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-05 23:22:04 -0300

    feat: use adapter to get auth method

--- src/Adapter.Api/Controllers/AutenticarController.cs
+++ src/Adapter.Api/Controllers/AutenticarController.cs
@@ -1,5 +1,5 @@
-﻿using Core.Negocio.Model;
-using Core.Negocio.Negocios;
+﻿using Adapter.Jwt;
+using Core.Negocio.Model;
 using Core.Negocios;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,21 +14,23 @@ namespace Adapter.Api.Controllers
     public class AutenticarController : ControllerBase
     {
         private readonly AppSettings _appSettings = new AppSettings();
+        private readonly ImplJwt _autenticarNegocio;
 
-        public AutenticarController(IOptions<AppSettings> options)
+        public AutenticarController(IOptions<AppSettings> options, ImplJwt autenticarNegocio)
         {
             _appSettings = options.Value;
+            _autenticarNegocio = autenticarNegocio;
         }
 
         [HttpPost("AutenticarUsuario")]
-        public IActionResult AutenticarUsuario([FromBody]UsuarioModel usuario)
+        public IActionResult AutenticarUsuario([FromBody] UsuarioModel usuario)
         {
             // Verifica se o usuário existe
             if (usuario == null)
                 return NotFound(new { message = "Usuário ou senha inválidos" });
 
             // Gera o Token
-            var token = AutenticarNegocio.GerarToken(usuario, _appSettings.Secret);
+            var token = _autenticarNegocio.GerarToken(usuario, _appSettings.Secret);
 
             // Retorna os dados
             return Ok(token);

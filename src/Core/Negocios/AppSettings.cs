9ace27ea12f0eca0b79cce23b98bd54feec1ada4
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-05 22:37:59 -0300

    refactor: move to adapter and use adapters

--- src/Core/Negocios/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Core.Negocio.Negocios
-{
-    public class AppSettings
-    {
-        public string connectionString { get; set; }
-        public string Secret { get; set; }  
-    }
-}

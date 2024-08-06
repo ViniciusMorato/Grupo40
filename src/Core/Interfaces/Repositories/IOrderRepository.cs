b36f06ea15b6c912c87708d6885d1c96fbf6b08c
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-05 21:39:55 -0300

    feat: add order repository interface

---
+++ src/Core/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Negocio.Model;
+
+namespace Core.Interfaces.Repositories
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<PedidoModel> GetOrders();
+        PedidoModel GetOrderById(Guid orderId);
+        void InsertOrder(PedidoModel order);
+        void UpdateOrder(PedidoModel order);
+        void DeleteOrderById(Guid idOrder);
+    }
+}

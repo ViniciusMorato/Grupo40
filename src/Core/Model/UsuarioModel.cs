e5ba5b031f31399e5942e458fd61406cb1667b3a
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-06 22:48:51 -0300

    feat: add constraint to user model

--- src/Core/Model/UsuarioModel.cs
+++ src/Core/Model/UsuarioModel.cs
@@ -1,10 +1,24 @@
-﻿namespace Core.Negocio.Model
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Enums;
+
+namespace Core.Model
 {
     public class UsuarioModel
     {
-        public int Id { get; set; }
+        private const string CpfPattern = @"[0-9]{3}\\.?[0-9]{3}\\.?[0-9]{3}\\-?[0-9]{2}";
+
+        [Key] [Required] public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo usuário é obrigatório")]
+        [RegularExpression(CpfPattern, ErrorMessage = "Campo precisa ser um CPF válido")]
         public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Campo senha é obrigatório")]
+        [MinLength(8, ErrorMessage = "Campo senha precisa conter no mínimo 8 caracteres")]
         public string Senha { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(AccessControl))]
         public string Papel { get; set; }
     }
 }

dc6dfab59904d7755c0e36528c831465040c7dcd
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-07 18:02:31 -0300

    feat: create dto to user auth

---
+++ src/Adapter.Api/DTO/UserAuthenticationDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Adapter.Api.DTO;
+
+public class UserAuthenticationDto
+{
+    private const string CpfPattern = @"[0-9]{3}\\.?[0-9]{3}\\.?[0-9]{3}\\-?[0-9]{2}";
+
+    [Required(ErrorMessage = "Campo usuário é obrigatório")]
+    [RegularExpression(CpfPattern, ErrorMessage = "Campo precisa ser um CPF válido")]
+    public string Usuario { get; set; }
+
+    [Required(ErrorMessage = "Campo senha é obrigatório")]
+    public string Senha { get; set; }
+}

6d4cb68a87c1e4b021889b798fa1b2e3d6c10627
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-07 18:06:36 -0300

    revert: remove private set

--- src/Core/Model/UsuarioModel.cs
+++ src/Core/Model/UsuarioModel.cs
@@ -7,18 +7,18 @@ namespace Core.Model
     {
         private const string CpfPattern = @"[0-9]{3}\\.?[0-9]{3}\\.?[0-9]{3}\\-?[0-9]{2}";
 
-        [Key] [Required] public int Id { get; private set; }
+        [Key] [Required] public int Id { get; set; }
 
         [Required(ErrorMessage = "Campo usuário é obrigatório")]
         [RegularExpression(CpfPattern, ErrorMessage = "Campo precisa ser um CPF válido")]
-        public string Usuario { get; private set; }
+        public string Usuario { get; set; }
 
         [Required(ErrorMessage = "Campo senha é obrigatório")]
         [MinLength(8, ErrorMessage = "Campo senha precisa conter no mínimo 8 caracteres")]
-        public string Senha { get; private set; }
+        public string Senha { get; set; }
 
         [Required]
         [EnumDataType(typeof(AccessControl))]
-        public AccessControl Papel { get; private set; }
+        public AccessControl Papel { get; set; }
     }
 }

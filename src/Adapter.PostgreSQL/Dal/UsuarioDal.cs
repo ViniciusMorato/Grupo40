987844d2030cf2df7cc9674c71da91a301942815
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-07 00:01:28 -0300

    feat: start implement user repo interface

---
+++ src/Adapter.PostgreSQL/Dal/UsuarioDal.cs
@@ -0,0 +1,32 @@
+﻿using Core.Interfaces.Repositories;
+using Core.Model;
+
+namespace Adapter.DataAccessLayer.Dal;
+
+public class UsuarioDal : IUserRepository
+{
+    public IEnumerable<UsuarioModel> GetUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public UsuarioModel GetUserById(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void InsertUser(UsuarioModel user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateUser(UsuarioModel user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteUserById(Guid idUser)
+    {
+        throw new NotImplementedException();
+    }
+}

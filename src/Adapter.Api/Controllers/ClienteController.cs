d8e836a4252e667e69e2a2a3208bc6a7a4fe0d82
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-07 22:45:28 -0300

    feat: implement client controller

--- src/Adapter.Api/Controllers/ClienteController.cs
+++ src/Adapter.Api/Controllers/ClienteController.cs
@@ -1,4 +1,7 @@
-﻿using AutoMapper;
+﻿using Adapter.Api.DTO;
+using AutoMapper;
+using Core.Entities;
+using Core.Interfaces.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,24 +13,36 @@ namespace Adapter.Api.Controllers
     public class ClienteController : ControllerBase
     {
         private readonly IMapper _mapper;
+        private readonly IUserService _userService;
 
-        public ClienteController(IMapper mapper)
+        public ClienteController(IMapper mapper, IUserService userService)
         {
             _mapper = mapper;
+            _userService = userService;
         }
 
-        [Authorize]
+        // [Authorize]
         [HttpPost("CadastrarCliente")]
-        public IActionResult CadastrarCliente()
+        public IActionResult CadastrarCliente([FromBody] AddUserDto user)
         {
-            return Ok();
+            Usuario userEntity = _mapper.Map<Usuario>(user);
+            _userService.AddNewUser(userEntity);
+
+            return CreatedAtAction(nameof(IdentificarCliente),
+                new { cpf = user.Cpf },
+                user);
         }
 
-        [Authorize]
-        [HttpGet("IdentificarCliente")]
-        public IActionResult IdentificarCliente()
+        // [Authorize] 
+        [HttpGet("{cpf}")]
+        public IActionResult IdentificarCliente(string cpf)
         {
-            return Ok();
+            var user = _userService.GetUserByCpf(cpf);
+
+            if (user != null) return NotFound();
+
+            UserDto userDto = _mapper.Map<UserDto>(user);
+            return Ok(userDto);
         }
     }
 }

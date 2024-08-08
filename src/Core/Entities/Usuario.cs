6675e528d6531af5f4b829a1f126ab6d716317b7
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-08 08:56:24 -0300

    refactor: remove message error from entity

--- src/Core/Entities/Usuario.cs
+++ src/Core/Entities/Usuario.cs
@@ -5,28 +5,24 @@ namespace Core.Entities
 {
     public sealed class Usuario
     {
-        private const string CpfPattern = @"^\d{3}\.?\d{3}\.?\d{3}\-?\d{2}$";
-
-        [Key] [Required] public int Id { get; set; }
+        [Key] public int Id { get; set; }
         
         [Required]
-        [MaxLength(100, ErrorMessage = "Nome não pode ultrapassar 100 caracteres")]
+        [MaxLength(100)]
         public string Nome { get; set; }
         
         [Required]
-        [MaxLength(100, ErrorMessage = "Sobrenome não pode ultrapassar 100 caracteres")]
+        [MaxLength(100)]
         public string SobreNome { get; set; }
 
-        [Required(ErrorMessage = "Campo usuário é obrigatório")]
-        [RegularExpression(CpfPattern, ErrorMessage = "Campo precisa ser um CPF válido: 123.123.123-12")]
+        [Required]
+        [MaxLength(14)]
         public string Cpf { get; set; }
 
-        [Required(ErrorMessage = "Campo senha é obrigatório")]
-        [MinLength(8, ErrorMessage = "Campo senha precisa conter no mínimo 8 caracteres")]
+        [Required]
         public string Senha { get; set; }
 
         [Required]
-        [EnumDataType(typeof(AccessControl))]
         public AccessControl Papel { get; set; }
     }
 }

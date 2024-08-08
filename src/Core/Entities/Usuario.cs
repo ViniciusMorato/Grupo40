45106b34044edd0c836aae3ecd2fd2bbce782c7b
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-07 22:50:03 -0300

    feat: add new fields

--- src/Core/Entities/Usuario.cs
+++ src/Core/Entities/Usuario.cs
@@ -8,6 +8,14 @@ namespace Core.Entities
         private const string CpfPattern = @"^\d{3}\.?\d{3}\.?\d{3}\-?\d{2}$";
 
         [Key] [Required] public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(100, ErrorMessage = "Nome não pode ultrapassar 100 caracteres")]
+        public string Nome { get; set; }
+        
+        [Required]
+        [MaxLength(100, ErrorMessage = "Sobrenome não pode ultrapassar 100 caracteres")]
+        public string SobreNome { get; set; }
 
         [Required(ErrorMessage = "Campo usuário é obrigatório")]
         [RegularExpression(CpfPattern, ErrorMessage = "Campo precisa ser um CPF válido: 123.123.123-12")]

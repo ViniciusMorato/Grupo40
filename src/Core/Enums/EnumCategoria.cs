1252a9d196c0debc34ff25291909359337e9a1eb
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-08-13 21:22:22 -0300

    feat: map enum category string

--- src/Core/Enums/EnumCategoria.cs
+++ src/Core/Enums/EnumCategoria.cs
@@ -1,9 +1,15 @@
-﻿namespace Core.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Enums;
 
 public enum EnumCategoria
 {
-    Lanche,
-    Acompanhamento,
-    Bebida,
-    Sobremesa
+    [Display(Name = "Lanche")]
+    Lanche = 1,
+    [Display(Name = "Acompanhamento")]
+    Acompanhamento = 2,
+    [Display(Name = "Bebida")]
+    Bebida = 3,
+    [Display(Name = "Sobremesa")]
+    Sobremesa = 4
 }
